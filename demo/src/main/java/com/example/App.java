1cbe2aa022a39cc504811b19ef827966bdd30452
Shweta <shwetapande58@gmail.com>
2025-08-17 19:39:16 +0530

    updated app.java for jenkin job 2

--- demo/src/main/java/com/example/App.java
+++ demo/src/main/java/com/example/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Helloo Feature-1 From First-App" );
+        System.out.println( "Hellooo Feature-1 From First-App" );
     }
 }
