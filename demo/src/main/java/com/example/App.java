f338962858ec4faaf5d1f64ebd20aec58b24ed6e
Shweta <shwetapande58@gmail.com>
2025-08-17 19:28:02 +0530

    update app.java again

--- demo/src/main/java/com/example/App.java
+++ demo/src/main/java/com/example/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Feature-1 From First-Apps" );
+        System.out.println( "Hello Feature-1 From First-App" );
     }
 }
